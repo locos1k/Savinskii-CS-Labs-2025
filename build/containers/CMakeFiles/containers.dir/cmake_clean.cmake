7ce57145fc0232f620cdeb06bed7eb8bc2285ccb
locos1k <andreysav2005@gmail.com>
2025-05-08 20:41:23 +0300

    final commit

---
+++ build/containers/CMakeFiles/containers.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/containers.dir/MySet.cpp.o"
+  "CMakeFiles/containers.dir/MySet.cpp.o.d"
+  "CMakeFiles/containers.dir/MyVector.cpp.o"
+  "CMakeFiles/containers.dir/MyVector.cpp.o.d"
+  "libcontainers.a"
+  "libcontainers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/containers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,12 +1,80 @@
-#include "AppRunner.hpp"
 #include <iostream>
+#include "containers/MySet.h"
+#include "containers/MyVector.h"
 
 int main() {
-    try {
-        AppRunner::RunApp();
-    } catch(const std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
-    
+    setlocale(LC_ALL, "Russian");
+
+    MyVector<char*> v("Hello!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    std::cout << "Вектор v: " << v << std::endl;
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    std::cout << "Вектор v: " << v << std::endl;
+    MyVector<char*> v1 = v;
+    std::cout << "Вектор v1: " << v1 << std::endl;
+    for (int i = 0; i < MAX_SIZE; i++)
+        v1.delete_element(0);
+    std::cout << "Вектор v1: " << v1 << std::endl;
+    MySet<const char*> s("Yes"), s1, s2;
+    s.add_element("Привет!");
+    s.add_element("No");
+    const char* str = "Hello!";
+    s.add_element(str);
+    std::cout << "Множество s: " << s << std::endl;
+    s1.add_element("Cat");
+    s1.add_element("No");
+    s1.add_element("Привет!");
+    std::cout << "Множество s1: " << s1 << std::endl;
+    s2 = s1 - s;
+    std::cout << "Множество s2=s1-s: " << s2 << std::endl;
+    std::cout << "Множество s1: " << s1 << std::endl;
+    std::cout << "Множество s: " << s << std::endl;
+    s2 = s - s1;
+    std::cout << "Множество s2=s-s1: " << s2 << std::endl;
+    std::cout << "Множество s1: " << s1 << std::endl;
+    std::cout << "Множество s: " << s << std::endl;
+    s2 = s1 + s;
+    std::cout << "Множество s2=s1+s: " << s2 << std::endl;
+    std::cout << "Множество s1: " << s1 << std::endl;
+    std::cout << "Множество s: " << s << std::endl;
+    s2 = s1 * s;
+    std::cout << "Множество s2=s1*s: " << s2 << std::endl;
+    std::cout << "Множество s1: " << s1 << std::endl;
+    std::cout << "Множество s: " << s << std::endl;
+    MySet<const char*> s3 = s2;
+    std::cout << "Множество s3=s2: " << s3 << std::endl;
+    if (s3 == s2)
+        std::cout << "Множество s3=s2\n";
+    else
+        std::cout << "Множество s3!=s2\n";
+    if (s3 == s1)
+        std::cout << "Множество s3=s1\n";
+    else
+        std::cout << "Множество s3!=s1\n";
+    if (s1 == s3)
+        std::cout << "Множество s1=s3\n";
+    else
+        std::cout << "Множество s1!=s3\n";
+
+    MySet<int> i1, i2;
+    i1.add_element(1);
+    i1.add_element(4);
+    i1.add_element(5);
+    i1.add_element(6);
+
+    i2.add_element(1);
+    i2.add_element(2);
+    i2.add_element(3);
+    i2.add_element(4);
+
+    std::cout << i1 + i2 << std::endl;
+    std::cout << i1 * i2 << std::endl;
+    std::cout << i1 - i2 << std::endl;
     return 0;
 }
