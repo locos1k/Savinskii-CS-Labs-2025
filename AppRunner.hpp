fb52e43a37e7ed9a2cf754765982bb08562e892e
locos1k <andreysav2005@gmail.com>
2025-05-29 00:09:22 +0300

    save

--- AppRunner.hpp
+++ AppRunner.hpp
@@ -1,5 +1,31 @@
 #pragma once
 
-namespace AppRunner {
-void RunApp();
-}  // namespace AppRunner
+#include "MyStack/MyStack.hpp"
+#include "Organizations/Factory.hpp"
+#include "Organizations/InsuranceCompany.hpp"
+#include "Organizations/Organization.hpp"
+#include "Organizations/ShipbuildingCompany.hpp"
+
+enum class Task {
+    Add = 1,
+    Print = 2,
+    Delete = 3,
+    Clear = 4,
+    Initialize = 5,
+    Exit = 6
+};
+
+enum class Type {
+    Insurance = 1,
+    Plant = 2,
+    Ship = 3
+};
+
+void print(MyStack::MyStack<Organization*>& container);
+bool remove(MyStack::MyStack<Organization*>& container, int index);
+void clear(MyStack::MyStack<Organization*>& container);
+
+void addElement(MyStack::MyStack<Organization*>& organizations);
+
+void demo(MyStack::MyStack<Organization*>& organizations);
+void menu();
--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include "AppRunner.hpp"
 
 int main() {
-    try {
-        AppRunner::RunApp();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
+    menu();
+    
+    return 0;
 }
